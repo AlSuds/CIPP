a5809c80f321c86914083220b832d17c5c23bd30
Alastair Sutherland <55421356+AlSuds@users.noreply.github.com>
2023-02-22 14:25:42 +1100

    Update adminRoutes.js

--- src/adminRoutes.js
+++ src/adminRoutes.js
@@ -6,10 +6,8 @@ const GDAPStatus = React.lazy(() => import('src/views/tenant/administration/List
 const GDAP = React.lazy(() => import('src/views/tenant/administration/GDAPWizard'))
 const appapproval = React.lazy(() => import('src/views/cipp/AppApproval'))
 const Psa_Ism_ListOrganizations = React.lazy(() => import('src/views/psa/ism/ListOrganizations'))
-// will add restricted ITS items here.
 const Psa_Ism_ListComputers = React.lazy(() => import('src/views/psa/ism/ListComputers'))
 const Psa_Ism_ListServers = React.lazy(() => import('src/views/psa/ism/ListServers'))
-// will restrict access to new ITS items here - before moving to the routes.js general area.
 
 const adminRoutes = [
   { path: '/cipp', name: 'CIPP' },
@@ -20,16 +18,8 @@ const adminRoutes = [
   { path: '/tenant/administration/appapproval', name: 'App Approval', component: appapproval },
   { path: '/tenant/administration/gdap-status', name: 'GDAP Status', component: GDAPStatus },
   { path: '/tenant/standards/apply-standard', name: 'Apply Standard', component: ApplyStandard },
-  {
-    path: '/psa/ism/orgs/list-organizations',
-    name: 'List Organizations',
-    component: Psa_Ism_ListOrganizations,
-  },
-  {
-    path: '/psa/ism/endpoints/list-computers',
-    name: 'List Computers',
-    component: Psa_Ism_ListComputers,
-  },
+  { path: '/psa/ism/orgs/list-organizations', name: 'List Organizations', component: Psa_Ism_ListOrganizations },
+  { path: '/psa/ism/endpoints/list-computers', name: 'List Computers', component: Psa_Ism_ListComputers },
   { path: '/psa/ism/endpoints/list-servers', name: 'List Servers', component: Psa_Ism_ListServers },
 ]
 
